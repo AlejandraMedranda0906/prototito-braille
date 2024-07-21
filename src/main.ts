df4d8b0d35629240b3d0b6ea8e488582639c4210
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-07-20 23:08:31 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
   const config = new DocumentBuilder()
   .setTitle('REST API')
   .setDescription('APpi de prototito braille')
